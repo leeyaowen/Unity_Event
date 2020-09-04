58918b49cfeb7eccd620f5d7952836d6d9a439e9
leeyaowen <jcjh30101@gmail.com>
2020-09-04 13:11:34 +0800

    btn play/pause video

---
+++ BTNevent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using Vuforia;
+
+public class BTNevent : MonoBehaviour
+{
+    public Button Vbtn;
+    GameObject vp;
+
+    void Start()
+    {
+        Button btn = Vbtn.GetComponent<Button>();
+        btn.onClick.AddListener(BtnClick);
+        vp = GameObject.Find("Video Player");
+    }
+
+    void BtnClick()
+    {
+        if (vp.GetComponent<VideoPlayer>().isPlaying)
+        {
+            vp.GetComponent<VideoPlayer>().Pause();
+        }
+        else
+        {
+            vp.GetComponent<VideoPlayer>().Play();
+        }
+    }
+}
